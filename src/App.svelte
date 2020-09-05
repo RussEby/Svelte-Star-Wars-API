85f2005f0d7f7938be4e1f32e8192b034c8638f0
Russell Eby <reby@ztr.biz>
2020-09-05 18:43:01 -0500

    Part 2 - Added the Store

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import PersonList from './components/PersonList.svelte';
 </script>
 
 <main>
 	<Header />
+	<PersonList />
 	<Footer />
 </main>
