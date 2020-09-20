09c5268c2b458d4deb581c59ab78866b1d6f4d90
Russell Eby <reby@ztr.biz>
2020-09-20 11:01:21 -0500

    Part-4-Button-Next-And-Previous

--- src/components/PersonList.svelte
+++ src/components/PersonList.svelte
@@ -2,6 +2,12 @@
     import StarWarsStore from '../store/StarWars';
     import Person from './Person.svelte';
     let allPersons = StarWarsStore.getAllPersons();
+
+    function loadList({ target }){
+        if(target.dataset.link != null){
+            allPersons = StarWarsStore.getByURL(target.dataset.link);
+        }
+    }
 </script>
 
 <div class="personList">
@@ -11,6 +17,20 @@
         {#each persons.results as person}
             <Person {person}/>
         {/each}
+        <div class="buttons">
+            <button 
+                data-link="{persons.previous}" 
+                disabled="{(persons.previous) ? '' : 'true'}" 
+                on:click={loadList}>
+                Previous 10
+            </button>
+            <button 
+                data-link="{persons.next}" 
+                disabled="{(persons.next) ? '' : 'true'}"  
+                on:click={loadList}>
+                Next 10
+            </button>
+        </div>
     {:catch error}
         <p>Whoops, there was an error! {JSON.stringify(error)} </p>
     {/await}
@@ -21,5 +41,23 @@
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
+    }
+    .buttons {
+        width: 100%;
+        text-align: center;
+    }
+    .buttons button {
+        padding: 10px 20px;
+        border-radius: 10px;
+        border: none;
+        outline: none;
+        margin: 5px;
+        background-color: #8185fc;
+        color: #222;
+        font-size: 1.5em;
+    }
+    button:disabled {
+        background-color: #8185fc;
+        color: #999;
     }
 </style>
